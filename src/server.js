648546196ee87b71d1980c57b9b5cb8bbd922dd2
Refri Rifwan <refririfwan@inovasi360.com>
2022-01-15 10:25:04 +0700

    data validation implemented

--- src/server.js
+++ src/server.js
@@ -1,11 +1,10 @@
 const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
 const notes = require('./api/notes');
 const NotesService = require('./services/inMemory/NotesService');
+const NotesValidator = require('./validator/notes');
 
 const init = async () => {
   const notesService = new NotesService();
-
   const server = Hapi.server({
     port: 5000,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
@@ -16,12 +15,11 @@ const init = async () => {
     },
   });
 
-  server.route(routes);
-
   await server.register({
     plugin: notes,
     options: {
       service: notesService,
+      validator: NotesValidator,
     },
   });
 
